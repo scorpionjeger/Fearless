242496c6ac623339baf5d9331c7f55ff8bbc2e04
scorpionjeger <scorpionjeger@hotmail.com>
2021-06-10 00:03:20 -0400

    update3

--- app.py
+++ app.py
@@ -17,30 +17,33 @@ typesL = ["Original", "Retweet"]
 types = ["Retweet"]
 typesL = ["Retweet"]
 ID = "A5_26"
-for i, typeses in enumerate(types):
-    DatesCollection = dill.load(open(ID + "3W" + typeses + ".pkd", "rb"))
-    # Binns=[datetime(2021,4,y,x,0,0)  for y in range(5,13) for x in range(24)]
-    # Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
-    Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
+if 0:
+    for i, typeses in enumerate(types):
+        DatesCollection = dill.load(open(ID + "3W" + typeses + ".pkd", "rb"))
+        # Binns=[datetime(2021,4,y,x,0,0)  for y in range(5,13) for x in range(24)]
+        # Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
+        Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
 
-    hist, bin = np.histogram(DatesCollection, Binns)
-    # print(hist)
-    # print(Binns)
-    Binns.pop()
+        hist, bin = np.histogram(DatesCollection, Binns)
+        # print(hist)
+        # print(Binns)
+        Binns.pop()
 
-    # DatesCollection=np.vectorize(lambda x:x.timestamp())
+        # DatesCollection=np.vectorize(lambda x:x.timestamp())
 
-    # plt.plot(Binns,hist,label=nstring)
-    Binns = np.array(Binns)
-    hist = np.array(hist)
-    #plt.plot(Binns - timedelta(hours=4), hist / 10, label=typesL[i])
-    # plt.plot(Binns-timedelta(hours=4), hist/1000000, label=typesL[i])
+        # plt.plot(Binns,hist,label=nstring)
+        Binns = np.array(Binns)
+        hist = np.array(hist)
+        #plt.plot(Binns - timedelta(hours=4), hist / 10, label=typesL[i])
+        # plt.plot(Binns-timedelta(hours=4), hist/1000000, label=typesL[i])
 
-
+Binns=range(10)
+hist=[x**2 for x in range(10)]
 #recompiling a dataframe for plotting
 pdDict={}
 #pdDict["Binns"]=Binns - timedelta(hours=4)
 pdDict["hist"]=hist
+pdDict["Binns"]=Binns
 Rpd=pd.DataFrame.from_dict(pdDict)
 
 
