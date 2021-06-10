98a61594c8f529d5dc4acea6b26c0a8b9f7a8f5f
scorpionjeger <scorpionjeger@hotmail.com>
2021-06-10 13:22:28 -0400

    update12

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ types = ["Retweet"]
 typesL = ["Retweet"]
 ID = "A5_26"
 print(ID)
-if 0:
+if 1:
     for i, typeses in enumerate(types):
-        DatesCollection = dill.load(open(ID + "3W" + typeses + ".pkd", "rb"))
+        DatesCollection = dill.load(open("data/"+ID + "3W" + typeses + ".pkd", "rb"))
         # Binns=[datetime(2021,4,y,x,0,0)  for y in range(5,13) for x in range(24)]
         # Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
         Binns = [datetime(2021, 4, y, x, 0, 0) for y in range(5, 26) for x in range(24)]
@@ -38,8 +38,8 @@ if 0:
         #plt.plot(Binns - timedelta(hours=4), hist / 10, label=typesL[i])
         # plt.plot(Binns-timedelta(hours=4), hist/1000000, label=typesL[i])
 if 1:
-    Binns=range(10)
-    hist=[x**2 for x in range(10)]
+    #Binns=range(10)
+    #hist=[x**2 for x in range(10)]
     #recompiling a dataframe for plotting
     pdDict={}
     #pdDict["Binns"]=Binns - timedelta(hours=4)
